64f810843f09c87e553b8e1a6930118cdc6bf718
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-02-22 03:43:33 +0600

    useMutations

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,27 @@ import "./App.css";
 import Post from "./Post";
 import PostsListOne from "./PostsListOne";
 import PostsListTwo from "./PostsListTwo";
-
+import CreatePost from "./CreatePost";
 
 function App() {
- const [currentPage, setCurrentPage] = useState(<PostsListOne/>)
-  
+  const [currentPage, setCurrentPage] = useState(<PostsListOne />);
+
   return (
     <div className="App">
-      <button onClick={()=>setCurrentPage(<PostsListOne/>)}>Post One</button>
-      <button onClick={()=>setCurrentPage(<PostsListTwo/>)}>Post Two</button>
-      <button onClick={()=>setCurrentPage(<Post id={1}/>)}>First Post</button>
-      <br /><br />
+      <button onClick={() => setCurrentPage(<PostsListOne />)}>Post One</button>
+      <button onClick={() => setCurrentPage(<PostsListTwo />)}>Post Two</button>
+      <button onClick={() => setCurrentPage(<Post id={1} />)}>
+        First Post
+      </button>
+      <button
+        onClick={() =>
+          setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)
+        }
+      >
+        Crete Post
+      </button>
+      <br />
+      <br />
       {currentPage}
     </div>
   );
---
+++ client/src/CreatePost.js
@@ -0,0 +1,46 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useRef } from "react";
+import { createPost } from "./api/posts";
+import Post from "./Post";
+
+const CreatePost = ({ setCurrentPage }) => {
+  const queryClient = useQueryClient();
+  const titleRef = useRef();
+  const bodyRef = useRef();
+  const createNewPost = useMutation({
+    mutationFn: createPost,
+    onSuccess: (data)=>{
+        queryClient.invalidateQueries(["posts"], {exact: true})
+        setCurrentPage(<Post id={data.id}/>)
+    },
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    createNewPost.mutate({
+      title: titleRef.current.value,
+      body: bodyRef.current.value,
+    });
+  }
+
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input id="title" ref={titleRef} />
+        </div>
+        <div>
+          <label htmlFor="body">Body</label>
+          <input id="body" ref={bodyRef} />
+        </div>
+        <button disabled={createNewPost.isLoading}>
+          {createNewPost.isLoading ? "Loading..." : "Create"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
